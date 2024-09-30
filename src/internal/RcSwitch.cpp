4ff27540df77a3b01d8382383cd7f1b2b8ba0df0
dac1e <wolfgang@die-schmieders.de>
2024-09-30 14:08:17 +0200

    Improved documentation in RcSwitch.cpp

--- src/internal/RcSwitch.cpp
+++ src/internal/RcSwitch.cpp
@@ -9,6 +9,66 @@
 
 namespace RcSwitch {
 
+/*
+ * The protocol is a stream of pulse pairs with different duration and pulse levels.
+ *
+ *   Normal level protocols start with a high level:
+ *         ___________________
+ *	   XXX|                   |____________________|XXXXX
+ *
+ *   Inverse level protocols start with a low level:
+ *                             ____________________
+ *	   XXX|___________________|                    |XXXXX
+ *
+ *	      ^                   ^                    ^
+ *        |1st pulse duration | 2nd pulse duration |
+ *
+ *
+ *  In the synchronization phase there is a short pulse followed by a very long pulse:
+ *     Normal level protocols:
+ *         ____
+ *     XXX|    |_____________________________________________________________|XXX
+ *
+ *     Inverse level protocols:
+ *              _____________________________________________________________
+ *     XXX|____|                                                             |XXX
+ *
+ *
+ *  In the data phase there is
+ *   a short pulse followed by a long pulse for a logical 0 data bit:
+ *     Normal level protocols:
+ *         __
+ *     XXX|  |________|XXX
+ *
+ *     Inverse level protocols:
+ *            ________
+ *     XXX|__|        |XXX
+ *
+ *   a long pulse followed by a short pulse for a logical 1 data bit:
+ *     Normal level protocols:
+ *         ________
+ *     XXX|        |__|XXX
+ *
+ *     Inverse level protocols:
+ *                  __
+ *     XXX|________|  |XXX
+ *
+ *
+ * The pulse duration specification for the different protocols are stored in 2 arrays:
+ *   normalLevelProtocolsTable[]
+ *  inverseLevelProtocolsTable[]
+ *
+ * Pulse durations sent out by a real world transmitter can vary. Hence the
+ * specification contains upper and lower boundaries for a pulse to be
+ * recognized as a valid synch. or data pulse.
+ *
+ * Synch. pulses and data pulses are typically a multiple of a protocol specific clock
+ * cycle. The specification tables contain already from the clock cycle pre-calculated
+ * durations to keep the interrupt handler quick. The lower / upper boundary tolerance
+ * is +- 20%.
+ *
+ */
+
 
 struct TimeRange {
 	uint32_t lowerBound;
@@ -52,9 +112,12 @@ struct Protocol {
 	PulseTypes pulseToPulseTypes(const Pulse &pulse) const;
 };
 
-/** Timings of the normal level protocols in microseconds: first synch pulse is high. */
+/** Timings of the normal level protocols in microseconds: */
 static const Protocol normalLevelProtocolsTable[] { // Sorted in ascending order of lowTimeRange.msecLowerBound
 		//     |synch                                                    |data
+		//                                                                |logical 0 data bit pulse pair                            |logical 1 data bit pulse pair
+        //      |low level pulse duration.. |high level pulse duration..  |low level pulse duration.. |high level pulse duration..  |low level pulse duration.. |high level pulse duration..
+        //      |..is the second pulse      |..is the first pulse         |..is the second pulse      |..is the first pulse         |..is the second pulse      |..is the first pulse
 		//#p    |lowerBound  |upperBound    |lowerBound  |upperBound      |lowerBound  |upperBound    |lowerBound  |upperBound      |lowerBound  |upperBound    |lowerBound  |upperBound
 		{    7,{{        7440,       11160},{         240,         360}},{{         720,        1080},{         120,         180}},{{         120,         180},{         720,        1080}}},
 		{    1,{{        8680,       13020},{         280,         420}},{{         840,        1260},{         280,         420}},{{         280,         420},{         840,        1260}}},
@@ -65,9 +128,13 @@ static const Protocol normalLevelProtocolsTable[] { // Sorted in ascending order
 		{    3,{{        5680,        8520},{        2400,        3600}},{{         880,        1320},{         320,         480}},{{         480,         720},{         720,        1080}}},
 };
 
-/** Timings of the normal level protocols in microseconds: first synch pulse is low. */
+/** Timings of the normal level protocols in microseconds: */
 static const Protocol inverseLevelProtocolsTable[] { // Sorted in ascending order of msecHighTimeLowerBound
 		//     |synch                                                    |data
+		//                                                                |logical 0 data bit pulse pair                            |logical 1 data bit pulse pair
+        //      |low level pulse duration.. |high level pulse duration..  |low level pulse duration.. |high level pulse duration..  |low level pulse duration.. |high level pulse duration..
+        //      |..is the first pulse       |..is the second pulse        |..is the first pulse       |..is the second pulse        |..is the first pulse       |..is the second pulse
+		//#p    |lowerBound  |upperBound    |lowerBound  |upperBound      |lowerBound  |upperBound    |lowerBound  |upperBound      |lowerBound  |upperBound    |lowerBound  |upperBound
 		//#p    |lowerBound  |upperBound    |lowerBound  |upperBound      |lowerBound  |upperBound    |lowerBound  |upperBound      |lowerBound  |upperBound    |lowerBound  |upperBound
 		{   13,{{         216,         324},{        7776,       11664}},{{         216,         324},{         432,         648}},{{         432,         648},{         216,         324}}},
 		{   11,{{         256,         384},{        9216,       13824}},{{         256,         384},{         512,         768}},{{         512,         768},{         256,         384}}},
